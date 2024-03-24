bc0caae40cb6e5dd47fccba6b6879de568c03bd0
KimJongHoss <haberde97@gmail.com>
2024-03-24 23:14:20 +0900

    4강 수강 완료

--- src/main/java/hg/jh/luko/ajaxtest/controller/AjaxController.java
+++ src/main/java/hg/jh/luko/ajaxtest/controller/AjaxController.java
@@ -3,6 +3,7 @@ package hg.jh.luko.ajaxtest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,5 +18,12 @@ public class AjaxController {
     public @ResponseBody String ex02() {
         System.out.println("AjaxController.ex02");
         return "Hello World"; // 리턴값이 그대로 출력됨
+    }
+
+    @GetMapping("/ex03")
+    public @ResponseBody String ex03(@RequestParam("param1") String param1,
+                                     @RequestParam("param2") String parma2) {
+        System.out.println("param1 = "+ param1 + ", param2 = " + parma2);
+        return "exo3 메서드 호출 완료";
     }
 }
